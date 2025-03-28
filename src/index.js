12f8b641a8a41a465aa433b8fa60bc838c789fa5
skorzany <177423232+skorzany@users.noreply.github.com>
2025-03-28 16:30:12 +0100

    Remove initial tests, set files to clean state

--- src/index.js
+++ src/index.js
@@ -1,21 +1,2 @@
 import "./reset.css";
 import "./styles.css";
-import { greeting } from "./greeting.js";
-import { compareAsc, format } from "date-fns";
-
-console.log(greeting);
-console.log(format(new Date(2014, 1, 11), "yyyy-MM-dd"));
-
-const dates = [
-    new Date(1995, 6, 2),
-    new Date(1987, 1, 11),
-    new Date(1989, 6, 10),
-];
-
-dates.sort(compareAsc);
-console.log(dates);
-
-document.addEventListener("click", () => {
-    console.log('clicked');
-    console.log(format(new Date(), "yyyy-MM-dd"));
-});
