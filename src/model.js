ea59aa6230f1d685ce903c79be6631af5aa0f1a0
skorzany <177423232+skorzany@users.noreply.github.com>
2025-04-14 15:34:18 +0200

    Simplify the initial model

--- src/model.js
+++ src/model.js
@@ -1,149 +1,90 @@
 class Task {
-    #title;
-    #description;
-    #dueDate;
-    #priority;
-    #completed;
-    #notes;
-
-    #fallbackTitle = "Default task title";
-    #fallbackDescription = 
-        "This is your task's description. keep it short, without much detail";
-    #fallbackDate = "1900-01-01";
-    #fallbackPriority = "normal";
-    #fallbackStatus = "false";
-    #fallbackNotes = 
-        "This is the place, where you can add notes to your task. " +
-        "Notes can be as long as you want and formatted as you want. " +
-        "The only limitation is that they gotta be text."
-
-    constructor({title, description, dueDate, priority, completed, notes}={}) {
-        this.#title = title || this.#fallbackTitle;
-        this.#description = description || this.#fallbackDescription;
-        this.#dueDate = dueDate || this.#fallbackDate;
-        this.#priority = priority || this.#fallbackPriority;
-        this.#completed = completed || this.#fallbackStatus;
-        this.#notes = notes || this.#fallbackNotes;
+    constructor({title="", description="", dueDate="1900-01-01",
+        priority="normal", completed="false", notes=""}={}) {
+        this._title = title;
+        this._description = description;
+        this._dueDate = dueDate;
+        this._priority = priority;
+        this._completed = completed;
+        this._notes = notes;
     }
 
     get title() {
-        return this.#title;
+        return this._title;
     }
 
     get description() {
-        return this.#description;
+        return this._description;
     }
 
     get dueDate() {
-        return this.#dueDate;
+        return this._dueDate;
     }
 
     get priority() {
-        return this.#priority;
+        return this._priority;
     }
 
     get completed() {
-        return this.#completed;
+        return this._completed;
     }
 
     get notes() {
-        return this.#notes;
+        return this._notes;
     }
 
     set title(value) {
-        this.#title = value;
+        this._title = value;
     }
 
     set description(value) {
-        this.#description = value;
+        this._description = value;
     }
 
     set dueDate(value) {
-        if (new Date(value).getDate() !== NaN) this.#dueDate = value;  
+        this._dueDate = value;  
     }
 
     set priority(value) {
-        const testValue = value.toString.toLowerCase();
-        if (['low', 'normal', 'high'].includes(testValue)) {
-            this.#priority = testValue;
-        }
+        this._priority = value;
     }
 
     set completed(value) {
-        const testValue = value.toString.toLowerCase();
-        if (['false', 'true'].includes(testValue)) this.#completed = testValue;
+        this._completed = value;
     }
 
     set notes(value) {
-        this.#notes = value;
+        this._notes = value;
     }
 }
 
 class Project {
-    #name;
-    #tasks;
-
-    constructor({name}={}) {
-        this.#name = name;
-        this.#tasks = [];
+    constructor({name=""}={}) {
+        this._name = name;
+        this._tasks = [];
     }
 
     get name() {
-        return this.#name;
+        return this._name;
     }
 
     get tasks() {
-        return this.#tasks;
+        return this._tasks;
     }
 
     set name(value) {
-        this.#name = value;
+        this._name = value;
     }
 
     addTask(task) {
-        this.#tasks.push(task);
+        this.tasks.push(task);
     }
 
     removeTask(taskIdx) {
-        this.#tasks.splice(taskIdx, 1);
+        this.tasks.splice(taskIdx, 1);
     }
 
     selectTask(taskIdx) {
-        return this.#tasks[taskIdx];
+        return this.tasks[taskIdx];
     }
 }
-
-class ProjectList {
-    #projects;
-
-    constructor() {
-        this.#projects = [];
-    }
-
-    get projects() {
-        return this.#projects;
-    }
-
-    addProject(project) {
-        this.projects.push([this.projects.length, project]);
-    }
-
-    removeProject(idx) {
-        this.#projects.map((v) => {
-            if(idx < v[0]) v[0] -= 1;
-        });
-        this.projects.splice(idx, 1);
-    }
-}
-
-
-// tests
-const myProjects = new ProjectList();
-myProjects.addProject('xyz');
-console.log(myProjects.projects);   // should be [[0, 'xyz']]
-myProjects.addProject('123');
-myProjects.addProject(new Project({name: "Project name",}));
-console.log(myProjects.projects);   // should be [[0, 'xyz], [1, '123'], [2, Project {name: "Project name"}]]
-console.log(myProjects.projects[2][1].name);   // should be "Project name"
-myProjects.removeProject(1);
-console.log(myProjects.projects);   // should be [[0, 'xyz'], [1, Project {name: "Project name"}]]
